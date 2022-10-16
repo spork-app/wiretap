30ed121136d03d61076f7816a4bec0e82a00c232
Austin Kregel <5355937+austinkregel@users.noreply.github.com>
2022-10-15 20:34:58 -0400

    Add basic test case.

---
+++ tests/TestCase.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Spork\Wiretap\Tests;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Orchestra\Testbench\TestCase as TestbenchTestCase;
+use Spork\Wiretap\WiretapServiceProvider.php;
+
+class TestCase extends TestbenchTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Factory::guessFactoryNamesUsing(
+            fn (string $modelName) => 'Spork\\Wiretap\\Database\\Factories\\'.class_basename($modelName).'Factory'
+        );
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            WiretapServiceProvider.php::class,
+        ];
+    }
+}
+
