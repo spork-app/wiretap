06e9454b1b32d34ffefff188d3c7330623d5ead7
Austin Kregel <5355937+austinkregel@users.noreply.github.com>
2022-11-24 23:46:11 -0500

    Update workflow and the config

--- src/WiretapServiceProvider.php
+++ src/WiretapServiceProvider.php
@@ -9,6 +9,7 @@ class WiretapServiceProvider extends ServiceProvider
 {
     public function register()
     {
+        $this->mergeConfigFrom(__DIR__ . '/../config/spork.php', 'spork.wiretap');
         Route::middleware('web')->group(__DIR__.'/../routes/web.php');
     }
 }
